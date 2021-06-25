21d68a380f599679b97ae8d0acf1c1cc09670919
Saidahmad <sataullayev@gmail.com>
2021-06-25 23:42:59 +0500

    camon camon

---
+++ app/src/main/java/com/example/a41_topshiriq/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.a41_topshiriq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class   MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                println("Click ")
+            }
+        })
+    }
+}
